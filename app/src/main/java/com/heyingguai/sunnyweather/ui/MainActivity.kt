d57a503fbd8aab0fc0426c59ec5de1f022efcd0b
chenyuedong-666 <59673773+chenyuedong-666@users.noreply.github.com>
2021-06-03 16:05:33 +0800

    first commit

--- app/src/main/java/com/heyingguai/sunnyweather/ui/MainActivity.kt
+++ app/src/main/java/com/heyingguai/sunnyweather/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.heyingguai.sunnyweather.ui
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.heyingguai.sunnyweather.R
 
@@ -9,5 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
